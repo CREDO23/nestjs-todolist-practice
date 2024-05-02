7a9c924c2ae4ce34b57d6ef86151709f01b79c68
CREDO23 <thierrybakera12@gmail.com>
2024-05-02 22:42:36 +0200

    feat: add tasks CRUD with in-memory storage

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { ITask } from './interfaces/task.interface';
+import { TasksService } from './tasks.service';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Post()
+  create(@Body() task: ITask) {
+    return this.tasksService.create(task.title, task.body);
+  }
+
+  @Get()
+  findAll() {
+    return this.tasksService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.tasksService.findOne(id);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updated: Partial<Omit<ITask, 'id'>>,
+  ) {
+    return this.tasksService.update(id, updated);
+  }
+
+  @Delete(':id')
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.tasksService.delete(id);
+  }
+}
